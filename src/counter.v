50b4e422ee8c849862d8ede20303f5bf16bd6ae1
Tormod <tormofl@stud.ntnu.no>
2025-03-04 12:06:53 +0100

    Change counter to 16 bit register, fix max value

--- src/counter.v
+++ src/counter.v
@@ -1,14 +1,14 @@
 module counter (
     input clk,
     input rst,
-    output reg [16:0] count
+    output reg [15:0] count
 );
 
     always @ (posedge clk)
     begin
         if (rst)
             count <= 0;
-        else if (count == 100000)
+        else if (count == 16'hFFFF)
             count <= 0;
         else
             count <= count+1;
