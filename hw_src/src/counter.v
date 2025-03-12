f94085ca0ff6a1598b981e6d9ab33d0708c75706
unknown <tormofl@stud.ntnu.no>
2025-03-12 14:36:32 +0100

    Add external uart module base state

--- hw_src/src/counter.v
+++ hw_src/src/counter.v
@@ -1,16 +1,26 @@
 module counter (
-    input clk,
-    input rst,
-    output reg [15:0] count
+    input i_Clk,
+    input i_Rst,
+    input i_Tx_Active,
+    output reg o_Tx_DV,
+    output reg [7:0] o_Count
 );
 
-    always @ (posedge clk)
+    always @ (posedge i_Clk)
     begin
-        if (rst)
-            count <= 0;
-        else if (count == 16'hFFFF)
-            count <= 0;
+        if (i_Rst)
+            begin
+                o_Count <= 0;
+                o_Tx_DV <= 0;
+            end
+        else if (!i_Tx_Active)
+            begin
+                o_Count <= (o_Count == 8'hFF) ? 0 : o_Count + 1;
+                o_Tx_DV <= 1; // Trigger UART
+            end
         else
-            count <= count+1;
+            begin
+                o_Tx_DV <= 0;
+            end
     end
 endmodule
--- hw_src/src/top.v
+++ hw_src/src/top.v
@@ -1,23 +1,38 @@
 module top (
     input CLK_50,
     input [0:0] SW,
-    output [15:0] count_out,
+//    output [7:0] count_out,
     output clk_out,
-    output uart_tx
+    output o_Tx_Serial,
+    output o_Rst
 );
 
+    wire [7:0]  w_Count;
+    wire        w_Tx_Active;
+    wire        w_Tx_Done;
+    wire        w_Tx_DV;
+
+    parameter BAUD_RATE = 115200;
+    parameter CLOCK_FREQ = 50000000;
+    parameter TICKS_PER_BIT = CLOCK_FREQ / BAUD_RATE;
+
     counter my_counter (
-        .clk(CLK_50),
-        .rst(SW),
-        .count(count_out)
+        .i_Clk(CLK_50),
+        .i_Rst(SW),
+        .i_Tx_Active(w_Tx_Active),
+        .o_Tx_DV(w_Tx_DV),
+        .o_Count(w_Count)
     );
 
-    uart_tx uart (
-        .clk(CLK_50),
-        .rst(SW),
-        .data(count_out[15:0]),
-        .tx(uart_tx)
+    uart_tx #(.CLKS_PER_BIT(TICKS_PER_BIT)) uart (
+        .i_Clock(CLK_50),
+        .i_Tx_DV(w_Tx_DV),
+        .i_Tx_Byte(w_Count),
+        .o_Tx_Active(w_Tx_Active),
+        .o_Tx_Serial(o_Tx_Serial),
+        .o_Tx_Done(w_Tx_Done)
     );
 
     assign clk_out = CLK_50;
+    assign o_Rst = SW;
 endmodule
