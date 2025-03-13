9f468e9dd9f9c42479e320a6d1dec5880de8f38b
unknown <tormofl@stud.ntnu.no>
2025-03-13 13:47:32 +0100

    Fix issue where counter starts at 16

--- hw_src/src/counter.v
+++ hw_src/src/counter.v
@@ -6,17 +6,19 @@ module counter (
     output reg [7:0] o_Count
 );
 
-    
+    reg [7:0] r_Count;
+
     always @ (posedge i_Clk)
     begin
         if (i_Rst)
             begin
-                o_Count <= 0;
+                //o_Count <= 0;
+                r_Count <= 0;
                 o_Tx_DV <= 0;
             end
         else if (!i_Tx_Active)
             begin
-                o_Count <= (o_Count == 8'hFF) ? 0 : o_Count + 1;
+                r_Count <= (o_Count == 8'hFF) ? 0 : o_Count + 1;
                 o_Tx_DV <= 1; // Trigger UART
             end
         else
@@ -24,4 +26,9 @@ module counter (
                 o_Tx_DV <= 0;
             end
     end
+    
+    always @ (posedge i_Clk)
+    begin
+        o_Count <= r_Count;
+    end 
 endmodule
